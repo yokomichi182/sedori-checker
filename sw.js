fff261c1fd03738e6be50ab9d49535ef64057047
yokomichi182 <yokomichi182@users.noreply.github.com>
2026-09-04 16:35:30 +0900

    sedori checker lite PWA

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='sedori-v1';
+const ASSETS=['./','./index.html','https://unpkg.com/@zxing/library@0.21.3/umd/index.min.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
