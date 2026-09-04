886557ae373d7404664b11e9fc5072db7e4a2f51
yokomichi182 <yokomichi182@users.noreply.github.com>
2026-09-04 16:49:54 +0900

    bump SW cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sedori-v1';
+const CACHE='sedori-v2';
 const ASSETS=['./','./index.html','https://unpkg.com/@zxing/library@0.21.3/umd/index.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
